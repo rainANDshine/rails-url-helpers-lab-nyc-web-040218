3331d6ba448420b27446fd033a9f30ce1a4e9071
Shun Yao <ysmiracle@hotmail.com>
2018-04-29 06:42:37 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = set_student
+  end
+
+  def activate
+    @student = set_student
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+    #binding.pry
+    redirect_to self.show
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 class Student < ActiveRecord::Base
+  after_initialize :set_default
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def set_default
+    self.active = false if self.active.nil?
+  end
+
+  def status
+    if self.active
+      "active"
+    else
+      "inactive"
+    end
   end
 end
