35eb5fb9c3eca30cd98aea465b61855bcd75a5af
Shun Yao <ysmiracle@hotmail.com>
2018-04-29 04:49:07 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ActiveRecord::Base
+  after_initialize :set_default
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def set_default
+    self.active ||= false
   end
 end
